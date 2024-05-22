d5f04042fba34410a2824dfb197edf437d4e2de7
magaramol <amoljb007@gmail.com>
2024-05-22 21:40:38 +0530

    window function

--- campusx.sql
+++ campusx.sql
@@ -15,10 +15,81 @@ order by mn desc, mn_rnk
 
 
 
+-------------------------
+
+
+select * ,last_value(name) over (
+partition by branch
+order by marks desc rows between unbounded preceding and unbounded following) as ac
+ from marks
+
+
+
+ ------------------
+
+-- find the branch topper
+
+select * from 
+(select * ,
+first_value(name) over (partition by branch order by marks
+--  desc rows between unbounded preceding and unbounded following
+) as nm,
+first_value(marks) over (partition by branch order by marks 
+-- desc rows between unbounded preceding and unbounded following 
+) as mrk
+from marks) t
+where t.marks=t.mrk
+and t.name=t.nm
+
+
+
+--------------------------------------------
+
+SELECT *
+FROM (
+    SELECT *,
+           FIRST_VALUE(name) OVER (PARTITION BY branch ORDER BY marks DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS nm,
+           FIRST_VALUE(marks) OVER (PARTITION BY branch ORDER BY marks DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS mrk
+    FROM marks
+) t
+WHERE t.marks = t.mrk
+  AND t.name = t.nm;
+
 
 -- crreate roll number from branch and marks
 
 
+
+
+
+
+
+
+--------------------------------------------------------
+
+select *, 
+lag(marks) over(partition by branch order by student_id),
+lead(marks) over(partition by branch order by student_id)  
+from marks;
+
+
+
+
+
+-----------------------
+
+
+------------ MOM
+
+select monthname(datee) , sum(amount),
+(
+(sum(amount)- lag(sum(amount)) over(order by month(datee)))/lag(sum(amount)) over(order by month(datee)))*100 as MOM
+from orders
+group by  MONTH(datee), MONTHNAME(datee)
+order by month(datee) 
+
+
+----------------
 
 
 
